90c5077b516b31191294202856f8ed1c0cb46e56
Snayderth Campo <snayderthdev@gmail.com>
2025-05-14 18:31:31 -0500

    Add Load in Data
    Test Load

--- src/data/songs.js
+++ src/data/songs.js
@@ -3,31 +3,37 @@ const songs = [
     author: "Silvestre Dangond",
     music: "Mi Amor Eres Tu",
     link: "https://www.youtube.com/watch?v=XS-7A6bdnNA",
+    load: "En proceso...",
   },
   {
     author: "Binomio de Oro",
     music: "Niña Bonita",
     link: "https://www.youtube.com/watch?v=Fa36CaBfONo",
+    load: "En proceso...",
   },
   {
     author: "Arcangel",
     music: "Hace Mucho Tiempo",
     link: "https://www.youtube.com/watch?v=WeCm7ufP6us",
+    load: "En proceso...",
   },
   {
     author: "Arcangel",
     music: "Llename de Luz",
     link: "https://www.youtube.com/watch?v=AXV5eDS2qXE",
+    load: "En proceso...",
   },
   {
     author: "Nigga, Flex",
     music: "Te Quiero",
     link: "https://www.youtube.com/watch?v=PtrBe-t1AfA",
+    load: "En proceso...",
   },
   {
     author: "Ed Sheeran",
     music: "Perfect",
     link: "https://www.youtube.com/watch?v=JcamjK-uNa4",
+    load: "En proceso...",
   },
 ];
 
