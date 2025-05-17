3ee92eeaf3733f8e683c6ee01d32ef518dc5111c
Snayderth Campo <snayderthdev@gmail.com>
2025-05-17 04:52:58 -0500

    Actualizar Regalos.jsx

--- src/components/Regalos.jsx
+++ src/components/Regalos.jsx
@@ -43,10 +43,10 @@ const Musics = ({ cancionesObj }) => {
     <ul>
       <li>
         <a
-          href={() => alert("En proceso")}
+          href="#"
           className="text-principal bg-slate-900 p-3 rounded-full text-center text-[12px] flex justify-center xl:text-[14px]"
         >
-          {cancionesObj.music} - {cancionesObj.author}
+          {cancionesObj.load}
         </a>
       </li>
     </ul>
