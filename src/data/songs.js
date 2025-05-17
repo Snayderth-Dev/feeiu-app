be60249b49f3074e0a424c654d80204d0fbdd6ca
Snayderth Campo <snayderthdev@gmail.com>
2025-05-17 16:35:19 -0500

    Delete other songs

--- src/data/songs.js
+++ src/data/songs.js
@@ -3,37 +3,45 @@ const songs = [
     author: "Chayanne",
     music: "Me Enamoré De Ti",
     link: "https://youtu.be/YJJbrBEZqy8",
-    load: "En proceso...",
+    //load: "En proceso...",
   },
   {
-    //author: "Binomio de Oro"
-    //music: "Niña Bonita"
-    //link: "https://www.youtube.com/watch?v=Fa36CaBfONo"
-    //load: "En proceso..."
+    //author: "Binomio de Oro",
+    //music: "Niña Bonita",
+    //link: "https://www.youtube.com/watch?v=Fa36CaBfONo",
+    //load: "En proceso...",
   },
   {
+    /*
     author: "Arcangel",
     music: "Hace Mucho Tiempo",
     link: "https://www.youtube.com/watch?v=WeCm7ufP6us",
     load: "En proceso...",
+    */
   },
   {
+    /*
     author: "Arcangel",
     music: "Llename de Luz",
     link: "https://www.youtube.com/watch?v=AXV5eDS2qXE",
     load: "En proceso...",
+    */
   },
   {
+    /*
     author: "Nigga, Flex",
     music: "Te Quiero",
     link: "https://www.youtube.com/watch?v=PtrBe-t1AfA",
     load: "En proceso...",
+    */
   },
   {
+    /*
     author: "Ed Sheeran",
     music: "Perfect",
     link: "https://www.youtube.com/watch?v=JcamjK-uNa4",
     load: "En proceso...",
+    */
   },
 ];
 
